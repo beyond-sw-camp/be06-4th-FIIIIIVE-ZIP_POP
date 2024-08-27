4a6485b9d41461b0cdab37d9fd27989370492105
dyun23 <ozsally@naver.com>
2024-08-27 11:30:51 +0900

    [Init] 프로젝트 생성

---
+++ src/main/java/com/example/senagae/SenagaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.senagae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenagaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenagaeApplication.class, args);
+    }
+
+}
