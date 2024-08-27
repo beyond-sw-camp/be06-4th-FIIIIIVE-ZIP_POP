cc71e31b62a6480fc409a9e10f4935beaa2dc802
dyun23 <ozsally@naver.com>
2024-08-27 12:03:09 +0900

    [Init] 프로잭트 생성

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.senagae;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SenagaeApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SenagaeApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.senagae;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SenagaeApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
